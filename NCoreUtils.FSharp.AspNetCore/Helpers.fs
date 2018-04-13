a414d3f5c4778dbd289bf253f7d37358f46ad596
Artyom <artyom.szasa@gmail.com>
2018-04-13 18:14:41 +0200

    TaskCanceledException handling, Service based JsonSerializerSettings.

--- NCoreUtils.FSharp.AspNetCore/Helpers.fs
+++ NCoreUtils.FSharp.AspNetCore/Helpers.fs
@@ -4,17 +4,25 @@ open Microsoft.AspNetCore.Http
 open Microsoft.Extensions.Primitives
 open NCoreUtils
 open System.Text
+open Newtonsoft.Json
 
 [<AutoOpen>]
 module Helpers =
 
   let private utf8 = UTF8Encoding false
 
-  let json (httpContext : HttpContext) obj =
+
+  let jsonWith (httpContext : HttpContext) (settings : JsonSerializerSettings) obj =
     let data =
-      Newtonsoft.Json.JsonConvert.SerializeObject obj
+      JsonConvert.SerializeObject (obj, settings)
       |> utf8.GetBytes
     let response = httpContext.Response
     response.Headers.["Content-Type"] <- StringValues "application/json; charset=utf-8"
     response.Headers.ContentLength    <- Nullable.mk data.LongLength
     response.Body.AsyncWrite data
+
+  let json (httpContext : HttpContext) obj =
+    let settings =
+      tryGetService<JsonSerializerSettings> httpContext.RequestServices
+      |> Option.defaultWith (fun () -> JsonSerializerSettings(ReferenceLoopHandling = ReferenceLoopHandling.Ignore))
+    jsonWith httpContext settings obj
--- NCoreUtils.FSharp.AspNetCore/RequestErrorHandlerMiddleware.fs
+++ NCoreUtils.FSharp.AspNetCore/RequestErrorHandlerMiddleware.fs
@@ -6,6 +6,7 @@ open Microsoft.AspNetCore.Http
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Logging
 open Microsoft.Extensions.Primitives
+open System.Threading.Tasks
 
 let run (httpContext : HttpContext) asyncNext = async {
   try do! asyncNext
@@ -15,5 +16,15 @@ let run (httpContext : HttpContext) asyncNext = async {
       httpContext.Response.Headers.Add ("X-Message", (StringValues : string -> _) exn.Message)
       HttpContext.setResponseStatusCode exn.StatusCode httpContext
       logger.LogDebug (exn, "Request cound not be processed.")
+    | :? TaskCanceledException as exn ->
+      match httpContext.RequestAborted.IsCancellationRequested with
+      | true ->
+        let logger = httpContext.RequestServices.GetRequiredService<ILoggerFactory>().CreateLogger "RequestErrorHandlerMiddleware"
+        logger.LogDebug (exn, "Request has been cancelled.")
+      | _ ->
+        let logger = httpContext.RequestServices.GetRequiredService<ILoggerFactory>().CreateLogger "RequestErrorHandlerMiddleware"
+        httpContext.Response.Headers.Add ("X-Message", (StringValues : string -> _) "Request timed out.")
+        HttpContext.setResponseStatusCode 408 httpContext
+        logger.LogDebug (exn, "Request timed out.")
     | exn ->
       ExceptionDispatchInfo.Capture(exn).Throw () }
