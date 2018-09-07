20097d5449cb6674fb61f507edb4bcd11474589f
Artyom Antyipin <artyom.szasa@gmail.com>
2018-09-07 13:22:08 +0200

    Sequence funs: tryMin/tryMax/tryMinBy/tryMaxBy, initial unit tests.

--- NCoreUtils.FSharp/Seq.fs
+++ NCoreUtils.FSharp/Seq.fs
@@ -1,7 +1,9 @@
 namespace NCoreUtils
 
 open System.Collections.Generic
+
 [<RequireQualifiedAccess>]
+[<CompiledName("Seq")>]
 module Seq =
 
   [<CompiledName("MapToArray")>]
@@ -19,3 +21,73 @@ module Seq =
       Seq.iteri (fun i item -> result.[i] <- mapper item) collection
       result
     | _ -> Seq.map mapper seq |> Seq.toArray
+
+  [<CompiledName("TryMin")>]
+  let tryMin (seq : seq<_>) =
+    use enumerator = seq.GetEnumerator ()
+    let mutable found = false
+    let mutable value = Unchecked.defaultof<_>
+    while enumerator.MoveNext () do
+      let current = enumerator.Current
+      match not found || value > current with
+      | true ->
+        value <- current
+        found <- true
+      | _ -> ()
+    match found with
+    | true -> Some value
+    | _    -> None
+
+  [<CompiledName("TryMinBy")>]
+  let tryMinBy selector (seq : seq<_>) =
+    use enumerator = seq.GetEnumerator ()
+    let mutable found = false
+    let mutable key = Unchecked.defaultof<_>
+    let mutable value = Unchecked.defaultof<_>
+    while enumerator.MoveNext () do
+      let current = enumerator.Current
+      let currentKey = selector current
+      match not found || key > currentKey with
+      | true ->
+        value <- current
+        key   <- currentKey
+        found <- true
+      | _ -> ()
+    match found with
+    | true -> Some value
+    | _    -> None
+
+  [<CompiledName("TryMax")>]
+  let tryMax (seq : seq<_>) =
+    use enumerator = seq.GetEnumerator ()
+    let mutable found = false
+    let mutable value = Unchecked.defaultof<_>
+    while enumerator.MoveNext () do
+      let current = enumerator.Current
+      match not found || value < current with
+      | true ->
+        value <- current
+        found <- true
+      | _ -> ()
+    match found with
+    | true -> Some value
+    | _    -> None
+
+  [<CompiledName("TryMaxBy")>]
+  let tryMaxBy selector (seq : seq<_>) =
+    use enumerator = seq.GetEnumerator ()
+    let mutable found = false
+    let mutable key = Unchecked.defaultof<_>
+    let mutable value = Unchecked.defaultof<_>
+    while enumerator.MoveNext () do
+      let current = enumerator.Current
+      let currentKey = selector current
+      match not found || key < currentKey with
+      | true ->
+        value <- current
+        key   <- currentKey
+        found <- true
+      | _ -> ()
+    match found with
+    | true -> Some value
+    | _    -> None
---
+++ NCoreUtils.Unit/Tests.fs
@@ -0,0 +1,41 @@
+﻿module Tests.Seq
+
+open System
+open Xunit
+open NCoreUtils
+
+let private rand = new System.Random()
+
+let private swap (a: _[]) x y =
+  let tmp = a.[x]
+  a.[x] <- a.[y]
+  a.[y] <- tmp
+
+// shuffle an array (in-place)
+let private shuffle a =
+  let a' = Array.copy a
+  Array.iteri (fun i _ -> swap a i (rand.Next(i, Array.length a'))) a'
+  a'
+
+[<Fact>]
+let ``min/max`` () =
+  let input = [| 1 .. 30 |]
+  let rand = shuffle input
+  Assert.Equal (None, Seq.tryMin [])
+  Assert.Equal (None, Seq.tryMax [])
+  Assert.Equal (Some 1, Seq.tryMin input)
+  Assert.Equal (Some 1, Seq.tryMin rand)
+  Assert.Equal (Some 30, Seq.tryMax input)
+  Assert.Equal (Some 30, Seq.tryMax rand)
+
+[<Fact>]
+let ``min/max with selector`` () =
+  let input = [| 1 .. 30 |] |> Array.map ref
+  let rand = shuffle input
+  let selector (x : _ ref) = x.Value
+  Assert.Equal (None, Seq.tryMinBy selector [])
+  Assert.Equal (None, Seq.tryMaxBy selector [])
+  Assert.Equal (Some (ref 1), Seq.tryMinBy selector input)
+  Assert.Equal (Some (ref 1), Seq.tryMinBy selector rand)
+  Assert.Equal (Some (ref 30), Seq.tryMaxBy selector input)
+  Assert.Equal (Some (ref 30), Seq.tryMaxBy selector rand)
